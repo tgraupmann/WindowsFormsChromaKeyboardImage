8fc411410825b9f0b9a2754814bfdc68c7187a14
Tim Graupmann <tgraupmann@gmail.com>
2017-05-09 18:55:34 -0700

    Initial commit of project

---
+++ Form1.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace WindowsFormsChromaKeyboardImage
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this._mButtonQuit = new System.Windows.Forms.Button();
+            this._mButtonLoadImage = new System.Windows.Forms.Button();
+            this._mPicture = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this._mPicture)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // _mButtonQuit
+            // 
+            this._mButtonQuit.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this._mButtonQuit.Location = new System.Drawing.Point(586, 459);
+            this._mButtonQuit.Name = "_mButtonQuit";
+            this._mButtonQuit.Size = new System.Drawing.Size(75, 23);
+            this._mButtonQuit.TabIndex = 0;
+            this._mButtonQuit.Text = "Quit";
+            this._mButtonQuit.UseVisualStyleBackColor = true;
+            this._mButtonQuit.Click += new System.EventHandler(this._mButtonQuit_Click);
+            // 
+            // _mButtonLoadImage
+            // 
+            this._mButtonLoadImage.Location = new System.Drawing.Point(12, 12);
+            this._mButtonLoadImage.Name = "_mButtonLoadImage";
+            this._mButtonLoadImage.Size = new System.Drawing.Size(75, 23);
+            this._mButtonLoadImage.TabIndex = 1;
+            this._mButtonLoadImage.Text = "Load Image";
+            this._mButtonLoadImage.UseVisualStyleBackColor = true;
+            this._mButtonLoadImage.Click += new System.EventHandler(this._mButtonLoadImage_Click);
+            // 
+            // _mPicture
+            // 
+            this._mPicture.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this._mPicture.Location = new System.Drawing.Point(12, 41);
+            this._mPicture.Name = "_mPicture";
+            this._mPicture.Size = new System.Drawing.Size(649, 412);
+            this._mPicture.TabIndex = 2;
+            this._mPicture.TabStop = false;
+            this._mPicture.Click += new System.EventHandler(this._mPicture_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(673, 494);
+            this.Controls.Add(this._mPicture);
+            this.Controls.Add(this._mButtonLoadImage);
+            this.Controls.Add(this._mButtonQuit);
+            this.Name = "Form1";
+            this.Text = "Chroma Image";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this._mPicture)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button _mButtonQuit;
+        private System.Windows.Forms.Button _mButtonLoadImage;
+        private System.Windows.Forms.PictureBox _mPicture;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,219 @@
+﻿using Corale.Colore.Core;
+using Corale.Colore.Razer.Keyboard;
+using System;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+using Color = Corale.Colore.Core.Color;
+
+namespace WindowsFormsChromaKeyboardImage
+{
+    public partial class Form1 : Form
+    {
+        const string KEY_CHROMA_IMAGE = "CHROMA_IMAGE";
+        const string KEY_IMAGE = "IMAGE";
+
+        private string _mFileName = string.Empty;
+
+        private class KeyData
+        {
+            public Key _mKey;
+            public Color _mColor;
+            public static implicit operator KeyData(Key key)
+            {
+                KeyData keyData = new KeyData();
+                keyData._mKey = key;
+                keyData._mColor = Color.Black;
+                return keyData;
+            }
+        }
+
+        #region Key layout
+
+        private static KeyData[,] _sKeys =
+        {
+            {
+                Key.Tab,
+                Key.Q,
+                Key.W,
+                Key.E,
+                Key.R,
+                Key.T,
+                Key.Y,
+                Key.U,
+                Key.I,
+                Key.O,
+                Key.P,
+                Key.Oem4, //[
+                Key.Oem5, //]
+                Key.Oem6, //\
+                Key.Delete,
+                Key.End,
+                Key.PageDown,
+                Key.Num7,
+                Key.Num8,
+                Key.Num9,
+            },
+            {
+                Key.CapsLock,
+                Key.A,
+                Key.S,
+                Key.D,
+                Key.F,
+                Key.G,
+                Key.H,
+                Key.J,
+                Key.K,
+                Key.L,
+                Key.Oem7, //;
+                Key.Oem8, //'
+                Key.Enter,
+                Key.Invalid,
+                Key.Invalid,
+                Key.Invalid,
+                Key.Invalid,
+                Key.Num4,
+                Key.Num5,
+                Key.Num6,
+            },
+            {
+                Key.LeftShift,
+                Key.Z,
+                Key.X,
+                Key.C,
+                Key.V,
+                Key.B,
+                Key.N,
+                Key.M,
+                Key.Oem9, //,
+                Key.Oem10, //.
+                Key.Oem11, //?
+                Key.RightShift,
+                Key.Invalid,
+                Key.Invalid,
+                Key.Invalid,
+                Key.Up,
+                Key.Invalid,
+                Key.Num1,
+                Key.Num2,
+                Key.Num3,
+            },
+        };
+
+        #endregion
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void _mButtonQuit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void _mButtonLoadImage_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+
+            openFileDialog1.InitialDirectory = "c:\\";
+            openFileDialog1.Filter = "image files (*.jpg)|*.jpg";
+            openFileDialog1.FilterIndex = 2;
+            openFileDialog1.RestoreDirectory = true;
+            if (!string.IsNullOrEmpty(_mFileName))
+            {
+                openFileDialog1.FileName = _mFileName;
+                openFileDialog1.InitialDirectory = Path.GetDirectoryName(_mFileName);
+            }
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                _mFileName = openFileDialog1.FileName;
+                _mPicture.Image = Image.FromFile(_mFileName);
+
+                Microsoft.Win32.RegistryKey key;
+                key = Microsoft.Win32.Registry.CurrentUser.CreateSubKey(KEY_CHROMA_IMAGE);
+                key.SetValue(KEY_IMAGE, _mFileName);
+                key.Close();
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            _mPicture.SizeMode = PictureBoxSizeMode.StretchImage;
+
+            Microsoft.Win32.RegistryKey key;
+            foreach (string name in Microsoft.Win32.Registry.CurrentUser.GetSubKeyNames())
+            {
+                if (name == KEY_CHROMA_IMAGE)
+                {
+                    key = Microsoft.Win32.Registry.CurrentUser.OpenSubKey(KEY_CHROMA_IMAGE);
+                    if (null != key)
+                    {
+                        _mFileName = (string)key.GetValue(KEY_IMAGE);
+                        if (!string.IsNullOrEmpty(_mFileName))
+                        {
+                            _mPicture.Image = Image.FromFile(_mFileName);
+                        }
+                    }
+                }
+            }
+        }
+
+        private static void SetColor(Key key, Color color)
+        {
+            if (key != Key.Invalid)
+            {
+                Keyboard.Instance.SetKey(key, color);
+            }
+        }
+
+        private void _mPicture_Click(object sender, EventArgs e)
+        {
+            Image image = _mPicture.Image;
+            if (null == image)
+            {
+                return;
+            }
+
+            Bitmap bitmap = image as Bitmap;
+            if (null == bitmap)
+            {
+                return;
+            }
+
+            MouseEventArgs evt = e as MouseEventArgs;
+            int minX = (int)(evt.X / (float)_mPicture.Width * bitmap.Width);
+            int minY = (int)(evt.Y / (float)_mPicture.Height * bitmap.Height);
+
+
+            /*
+            if (minX < bitmap.Width &&
+                minY < bitmap.Height)
+            {
+                bitmap.SetPixel(minX, minY, System.Drawing.Color.Red);
+                _mPicture.Image = bitmap;
+            }
+            */
+
+            int x = minX;
+            for (int i = 0; i < _sKeys.GetLength(0); ++i, ++x)
+            {
+                int y = minY;
+                for (int j = 0; j < _sKeys.GetLength(1); ++j, ++y)
+                {
+                    System.Drawing.Color c1 = System.Drawing.Color.Black;
+                    if (x < bitmap.Width &&
+                        y < bitmap.Height)
+                    {
+                        c1 = bitmap.GetPixel(x, y);
+                    }
+                    KeyData keyData = _sKeys[i, j];
+                    keyData._mColor = new Color(c1.R, c1.G, c1.B);
+                    SetColor(keyData._mKey, keyData._mColor);
+                }
+                ++minX;
+            }
+        }
+    }
+}
